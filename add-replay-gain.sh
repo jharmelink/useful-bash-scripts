8f2e3e6cc475a9601a19e005c338696e9f43ad19
Jan Willem Harmelink <677156+jharmelink@users.noreply.github.com>
2020-10-27 13:47:39 +0100

    Added params

--- add-replay-gain.sh
+++ add-replay-gain.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
-find . -mindepth 2 -type d  | while read dir
+#!/usr/bin/env bash
+
+ROOT=${1-.}
+DEPTH=${2-1}
+
+find ${ROOT} -mindepth ${DEPTH} -type d | while read DIR
 do
-    echo "Adding replay gain to ${dir}/*.flac..."
-    metaflac --add-replay-gain "${dir}/"*.flac
+    echo "Adding replay gain to ${DIR}/*.flac..."
+    metaflac --add-replay-gain "${DIR}/"*.flac
 done
